d4c2228718afaa959703e7510d26abd50f685ffc
Bianca <biancabezerrathe71@gmail.com>
2024-01-24 18:29:56 -0300

    Conexão entre nível 2 e 3

--- levels/scripts/level2.gd
+++ levels/scripts/level2.gd
@@ -4,10 +4,12 @@ var allowed_levelup = false
 @onready var player = get_node("/root/GaloFrito")
 
 func _on_area_2d_body_entered(body):
+	print("yesss")
 	if body.has_method("player") and global.level2_completed == true:
-			allowed_levelup = true
-			$WaitTime.start()
-			global.level2_completed = false
+		print("yesss queen")
+		allowed_levelup = true
+		$WaitTime.start()
+		global.level2_completed = false
 
 func _on_wait_time_timeout():
 	get_tree().change_scene_to_file("res://levels/scenes/level3.tscn")
